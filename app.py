3e161b1c1b0c31d4e7b16553b8f0c23690788159
saifulla-git <saifkhan999246049@gmail.com>
2026-02-28 20:02:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -259,16 +259,16 @@ elif menu == "Admin Panel":
 
                 st.success("Meeting saved and activated.")
                 st.rerun()
-
-        if current_status == "Active":
-
-    if st.button("Close Current Meeting"):
-
-        meeting_id = current_meeting_id
+      if current_status == "Active":
+          if st.button("Close Current Meeting"):meeting_id = current_meeting_id
 
         votes = db.collection("meeting_details") \
             .where("meeting_id", "==", meeting_id) \
             .stream()
+
+    
+
+        
 
         # rest of your logic here
 
