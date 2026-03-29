27c844937acca7567eb5af73141e3e6043ce5260
saifulla-git <saifkhan999246049@gmail.com>
2026-03-29 20:18:10 +0530

    Update database.py

--- database.py
+++ database.py
@@ -3,7 +3,7 @@ from firebase_admin import credentials, firestore
 
 # This connects the Brain to your Firebase data
 if not firebase_admin._apps:
-   cred = credentials.Certificate("firebase.json")
+    cred = credentials.Certificate("firebase.json")
     firebase_admin.initialize_app(cred)
 
 db = firestore.client()
